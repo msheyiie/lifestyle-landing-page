66e9ef7645e9fcafef3e6700400ab2f6190409da
seyi <seyi@gmail.com>
2020-05-12 14:10:06 +0100

    initial commit

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+document.querySelector("#mob-nav").addEventListener("click", function(){
+  document.querySelector(".sidebar").classList.toggle("show"); 
+  document.body.classList.toggle("body");
+  })
+
+document.querySelector("#close").addEventListener("click", function(){
+  document.querySelector(".sidebar").classList.toggle("show"); 
+  document.body.classList.toggle("body");
+  })
+
